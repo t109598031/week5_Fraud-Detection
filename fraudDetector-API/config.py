c0c7b4f56d68f69e529da61226606067335c7e26
whyurso87 <42568009+whyurso87@users.noreply.github.com>
2021-03-31 20:28:39 +0800

    Create config.py

---
+++ fraudDetector-API/config.py
@@ -0,0 +1,8 @@
+aws_access_key_id = ''
+aws_secret_access_key = ''
+region_name = 'us-west-2'
+AWS_S3_BUCKET = ""
+AWSstaffRecord = ""
+AWSnotMemberRecord = ""
+fraudDetectorAPIname = ""
+fraudDetctorEventName = ""
